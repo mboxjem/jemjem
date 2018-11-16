f9c16651cbe87f86bd675d3458ae1de38121bce3
mboxjem <mboxjem@gmail.com>
2018-11-16 15:38:30 +0700

    Add files via upload

---
+++ NR.lua
@@ -0,0 +1,14 @@
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("2.3510621e-38;9.5::",
+  gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("2.3510621e-38", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  if gg.getResultCount() <= 20 then
+  gg.toast("Value Found")
+  gg.getResults(20)
+  gg.editAll("0", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("No recoil Ok")
+  else
+  gg.clearResults()
+  gg.alert("Value Not Found, Run Script Again Or Restart The Game")
+  end
