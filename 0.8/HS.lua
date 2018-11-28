a7eec799fc600f5e160ca39d37acb5652721d870
mboxjem <mboxjem@gmail.com>
2018-11-29 04:21:27 +0700

    Update HS.lua

--- 0.8/HS.lua
+++ 0.8/HS.lua
@@ -1,9 +1,9 @@
   gg.setRanges(gg.REGION_ANONYMOUS)
   gg.searchNumber("9.201618;30.5;25", gg.TYPE_FLOAT)
   gg.searchNumber("30.5;25", gg.TYPE_FLOAT)
-  if gg.getResultCount() <= 100 then
+  if gg.getResultCount() <= 10 then
     gg.toast("Value Found")
-    gg.getResults(100)
+    gg.getResults(10)
     gg.editAll("250", gg.TYPE_FLOAT)
     gg.toast("Head Shoot Ok")
     gg.clearResults()
