fcb64d0333cc776527870801e78fc309efe5b971
mboxjem <mboxjem@gmail.com>
2018-11-29 04:19:48 +0700

    Update MB.lua

--- 0.8/MB.lua
+++ 0.8/MB.lua
@@ -1,6 +1,6 @@
   gg.setRanges(gg.REGION_ANONYMOUS)
-  gg.searchNumber("1;20.51941871643;2.04908943176;-86.45767974854;-92.2311706543;16.0;26.0;18.0;28.0;8.0;15.0::", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
-  gg.searchNumber("16.0;26.0;18.0;28.0;8.0;15.0", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1;20.51941871643;2.04908943176;-86.45767974854;-92.2311706543;16.0::", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("16.0", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
   if gg.getResultCount() <= 10 then
     gg.toast("Value Found")
     gg.getResults(10)
