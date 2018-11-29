4d00184bd46e3fee0ea9a8dbcba57bbbdcdef1b4
mboxjem <mboxjem@gmail.com>
2018-11-29 20:03:11 +0700

    Add files via upload

---
+++ 0.8/SR3.lua
@@ -0,0 +1,13 @@
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("1.0F;0.0001F;20.0F;0.00050000002F;0.40000000596F::17", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  if gg.getResultCount() <= 100 then
+    gg.toast("Value Found")
+    gg.getResults(100)
+    gg.editAll("2", gg.TYPE_FLOAT)
+    gg.clearResults()
+    gg.toast("Speed Run High Ok")
+  else
+    gg.clearResults()
+    gg.alert("Value Not Found, Run Script Again Or Restart The Game")
+  end
