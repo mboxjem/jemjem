f5d08581056a027173a2af28fe67a04367303473
mboxjem <mboxjem@gmail.com>
2018-12-19 05:55:29 +0700

    Add files via upload

--- Aim/AB.lua
+++ Aim/AB.lua
@@ -1,13 +1,27 @@
-  gg.setRanges(gg.REGION_CODE_APP)
-  gg.searchNumber("360.0;0.0001;1,478,828,288.0::", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("30;30;0.3;0.3;80;25;220::", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
   gg.getResultCount()
-  gg.searchNumber("0.0001", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("0.3", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
   if gg.getResultCount() <= 20 then
     gg.toast("Value Found")
     gg.getResults(20)
-    gg.editAll("1.0e10", gg.TYPE_FLOAT)
+    gg.editAll("0.03", gg.TYPE_FLOAT)
     gg.clearResults()
-    gg.toast("Aim Lock Ok")
+    gg.toast("50%")
+  else
+    gg.clearResults()
+    gg.alert("Value Not Found, Run Script Again Or Restart The Game")
+  end
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("36D;1;50528513D;1203982336D:", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResultCount()
+  gg.searchNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  if gg.getResultCount() <= 20 then
+    gg.toast("Value Found")
+    gg.getResults(20)
+    gg.editAll("10", gg.TYPE_FLOAT)
+    gg.clearResults()
+    gg.toast("Aim Bot Ok")
   else
     gg.clearResults()
     gg.alert("Value Not Found, Run Script Again Or Restart The Game")
