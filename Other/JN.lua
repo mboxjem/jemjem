5d3999e62f89100ca906d6658c0deac79414fff3
mboxjem <mboxjem@gmail.com>
2019-03-15 13:49:30 +0700

    Add files via upload

---
+++ Other/JN.lua
@@ -0,0 +1,13 @@
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("1D;1.0;35.0;443.0;0.0;55.0;0.57357645035;1D::29", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1;443", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  if gg.getResultCount() <= 50 then
+    gg.toast("Value Found")
+    gg.getResults(50)
+    gg.editAll("3;2500", gg.TYPE_FLOAT)
+    gg.toast("Jump No Fall Damage Ok")
+    gg.clearResults()
+  else
+    gg.clearResults()
+    gg.alert("Value Not Found, Run Script Again Or Restart The Game")
+  end
