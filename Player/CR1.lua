8ca5a6213a9c98a8a55152e4835e4af73c9b2872
mboxjem <mboxjem@gmail.com>
2019-06-13 08:03:55 +0700

    Update CR1.lua

--- Player/CR1.lua
+++ Player/CR1.lua
@@ -1,4 +1,4 @@
-  gg.setRanges(bit32.bxor(gg.REGION_VIDEO, gg.REGION_BAD))
+  gg.setRanges(gg.REGION_VIDEO)
   gg.searchNumber("655,370;1,080,033,292;8,200;1,194,380,045;786,441;1,661,239,308;671,236,103::25", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
   gg.searchNumber("8200", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
   if gg.getResultCount() <= 50 then
