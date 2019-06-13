27642f82da60c3ed0da3d2b8e88a2f818a311517
mboxjem <mboxjem@gmail.com>
2019-06-13 08:04:35 +0700

    Update WH.lua

--- Player/WH.lua
+++ Player/WH.lua
@@ -1,4 +1,4 @@
-  gg.setRanges(gg.REGION_BAD)
+  gg.setRanges(gg.REGION_VIDEO)
   gg.searchNumber("135,215D;4,140D;3.7615819e-37;2::", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
   gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
   if gg.getResultCount() <= 50 then
@@ -10,6 +10,7 @@
     gg.clearResults()
     gg.alert("Value Not Found, Run Script Again")
   end
+  gg.setRanges(gg.REGION_VIDEO)
   gg.searchNumber("194D;3.7615819e-37;2;-1;1;-127::", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
   gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
   if gg.getResultCount() <= 50 then
