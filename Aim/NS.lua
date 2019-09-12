f11a3deeedf73a81223074c19191bc0c7413a1a2
mboxjem <mboxjem@gmail.com>
2019-09-12 21:19:38 +0700

    Create NS.lua

---
+++ Aim/NS.lua
@@ -0,0 +1,12 @@
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber(':CameraShakeTemplate', gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1)
+  if gg.getResultCount() <= 200 then
+    gg.toast("Value Found")
+    gg.getResults(200)
+    gg.editAll(':', gg.TYPE_BYTE)
+    gg.clearResults()
+    gg.toast("No Shake Ok")
+  else
+    gg.clearResults()
+    gg.alert("Value Not Found, Run Script Again Or Restart The Game")
+  end
