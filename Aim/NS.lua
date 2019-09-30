61fa8d14857483a2a607aad4f0ecc05e14d3edb5
mboxjem <mboxjem@gmail.com>
2019-09-30 18:06:51 +0700

    Update NS.lua

--- Aim/NS.lua
+++ Aim/NS.lua
@@ -10,3 +10,15 @@
     gg.clearResults()
     gg.alert("Value Not Found, Run Script Again Or Restart The Game")
   end
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber(':AnimEndTime', gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1)
+  if gg.getResultCount() <= 200 then
+    gg.toast("Value Found")
+    gg.getResults(200)
+    gg.editAll(':', gg.TYPE_BYTE)
+    gg.clearResults()
+    gg.toast("No Shake Ok")
+  else
+    gg.clearResults()
+    gg.alert("Value Not Found, Run Script Again Or Restart The Game")
+  end
