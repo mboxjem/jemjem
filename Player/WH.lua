cfbb639d2d0dd414a8634e2420df971020380103
mboxjem <mboxjem@gmail.com>
2019-10-24 22:00:40 +0700

    Update WH.lua

--- Player/WH.lua
+++ Player/WH.lua
@@ -1,5 +1,5 @@
   gg.setRanges(gg.REGION_VIDEO)
-  gg.searchNumber("135,215D;4,140D;3.7615819e-37;2::", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("5.79227989e21;5.80137564e-42;4.74081492e21;-5.56955884e-40;4.81460302e21;3.76158192e-37;2.0::73", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
   gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
   if gg.getResultCount() <= 50 then
     gg.toast("Value Found")
@@ -11,7 +11,7 @@
     gg.alert("Value Not Found, Run Script Again")
   end
   gg.setRanges(gg.REGION_VIDEO)
-  gg.searchNumber("194D;3.7615819e-37;2;-1;1;-127::", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("4.79759574e21;2.71851902e-43;1.39125666e-19;3.9236357e-44;1.1202056e-19;3.76158192e-37;2.0::77", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
   gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
   if gg.getResultCount() <= 50 then
     gg.toast("Value Found")
