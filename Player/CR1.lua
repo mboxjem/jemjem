50b03367794f07f7df4abc7c04df0ca51126156a
mboxjem <mboxjem@gmail.com>
2020-03-07 21:45:01 +0700

    Update CR1.lua

--- Player/CR1.lua
+++ Player/CR1.lua
@@ -1,5 +1,5 @@
   gg.setRanges(gg.REGION_VIDEO)
-  gg.searchNumber("1.391252e-19F;1.10204137e-39F;2.46264371e21F;8200D;45.259.05078125F;7.23032999e-15F;2.2509765625::25", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1.391252e-19F;1.10204137e-39F;2.46264371e21F;8200;45.259.05078125F;7.23032999e-15F;2.2509765625::25", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
   gg.searchNumber("8200", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
   if gg.getResultCount() <= 50 then
     gg.toast("Value Found")
