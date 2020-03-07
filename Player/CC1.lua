05f0b2375b0609e6e76e1132be031f688fe1786a
mboxjem <mboxjem@gmail.com>
2020-03-07 21:52:37 +0700

    Update CC1.lua

--- Player/CC1.lua
+++ Player/CC1.lua
@@ -1,5 +1,5 @@
   gg.setRanges(gg.REGION_VIDEO)
-  gg.searchNumber("2.49031467e21F;1.56945428e-43F;1.39125265e-19F;8200D;45.259.0625F;9.19023376e-29F;3.75391101837:25", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("2.49031467e21F;1.56945428e-43F;1.39125265e-19F;8200D;45.259.0625F;9.19023376e-29F;3.75391101837F:25", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
   gg.searchNumber("8200", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
   if gg.getResultCount() <= 50 then
     gg.toast("Value Found")
