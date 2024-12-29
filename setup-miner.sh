ff0441e173ccc9399c2cca0d475bf0fa6200f19e
mboxjem <mboxjem@gmail.com>
2024-12-29 10:04:54 +0700

    Create setup-miner.sh

---
+++ setup-miner.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <pool> <wallet> <worker> <password>"
+    exit 1
+fi
+
+POOL=$1
+WALLET=$2
+WORKER=$3
+PASSWORD=$4
+
+apt update -y
+apt install wget tar libsodium-dev -y
+
+cd /tmp || exit
+
+if wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz; then
+    tar -xf hellminer_linux64.tar.gz
+    ./hellminer -c "$POOL" -u "$WALLET.$WORKER" -p "$PASSWORD"
+else
+    echo "Failed to download hellminer"
+    exit 1
+fi
